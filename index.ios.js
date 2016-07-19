c40a75c27b8d58b63be02894fc23ff97ead5f563
Chris Weeks <chris.weeks@internations.org>
2016-07-20 01:35:12 +0200

    created first view using navigator

--- index.ios.js
+++ index.ios.js
@@ -13,31 +13,25 @@ import {
     View
 } from 'react-native';
 
+import Main from './App/Components/Main';
+
 class githubNative extends Component {
     render() {
         return (
-
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: 'Github Notetaker',
+                    component: Main
+                }} />
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+        flex: 1
+    }
 });
 
 AppRegistry.registerComponent('githubNative', () => githubNative);
